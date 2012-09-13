269b17368638ed773403b7798cd517a6b93e39bd
Jason Mabry <jmabry@mymabry.com>
2012-09-13 10:12:43 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GovSchoolApplication::Application.config.secret_token = '67e4e8b595280c526b56f856256b91b799e8b4f5183395fff191081ef223be5992d3fb89d7d96b631b6fc8e16e5d67dbdd7681c74f15e1778100a92451c42b53'
