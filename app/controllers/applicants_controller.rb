f71896fa3efdc6fbfd76dd9c560b805d40ff75be
Jason Mabry <jmabry@mymabry.com>
2012-09-13 16:56:04 -0400

    added flash

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -6,6 +6,7 @@ class ApplicantsController < ApplicationController
   def create
     @applicant = Applicant.new(params[:applicant])
     if @applicant.save
+      flash[:success] = "Your information was saved."
       redirect_to new_applicant_path
     end
   end
