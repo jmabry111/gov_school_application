dd42b40285d0e4a904e34e11f23427d97bc82583
Jason Mabry <jmabry@mymabry.com>
2012-10-01 16:31:19 -0400

    adds all applicants view, chages edit to make all fields editiable

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -2,6 +2,10 @@ class ApplicantsController < ApplicationController
   
   before_filter :authenticate_user!, :except => [:new, :show]
   
+  def index
+    @applicants = Applicant.paginate(page: params[:page])
+  end
+  
   def new
     @applicant = Applicant.new
     @school = [School.list].flatten
@@ -18,7 +22,6 @@ class ApplicantsController < ApplicationController
     respond_to do |format|
       if @applicant.save
         NotificationsMailer.new_message(@applicant).deliver
-        flash[:success] = "Message was successfully sent."
         format.html { redirect_to @applicant, notice: 'Information was successfully submitted.' }
         format.json { render json: @applicant, status: :created, location: @applicant }
       else
