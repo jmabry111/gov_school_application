c4c0353746359b803491f567e9a90c22a1729aca
Jason Mabry <jmabry@mymabry.com>
2012-10-12 14:07:29 -0400

    adds has_many and belongs_to to school and user

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,6 +2,7 @@ class School < ActiveRecord::Base
   
   attr_accessible :name, :representative_email
   has_many :applicants
+  has_one :user
 
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
