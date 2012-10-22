92dcd2b908bf63b5e75be3b150dff2720c060197
Jason Mabry <jmabry@mymabry.com>
2012-10-22 14:46:32 -0400

    adds print.css for printing forms

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,6 @@ GovSchoolApplication::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  config.assets.precompile += %w( application-print.css application.css)
 end
