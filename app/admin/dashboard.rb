07364f8390e5ea38a98e35277817dd461298f939
Jason Mabry <jmabry@mymabry.com>
2012-10-30 10:31:32 -0400

    get rid of dashboards for now

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,12 +3,12 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-#    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-#      span :class => "blank_slate" do
-#        span "Welcome to Active Admin. This is the default dashboard page."
-#        small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
-#      end
-#    end
+    div :class => "blank_slate_container", :id => "dashboard_default_message" do
+      span :class => "blank_slate" do
+        span "Welcome to Active Admin. This is the default dashboard page."
+        small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
+      end
+    end
   
 #  columns do
     
@@ -25,25 +25,24 @@ ActiveAdmin.register_page "Dashboard" do
 #    end
 #  end
   
-  columns do
-    column do
-      panel "List of Schools" do
-        table_for School.order('name') do
-          column("School Name") {|school| school.name}
-        end
-      end
-    end
+#  columns do
+#    column do
+#        table_for School.order('name') do
+#          column("School Name") {|school| school.name}
+#        end
+#      end
+#    end
     
-    column do
-      panel "Current Users" do
-        table_for User.order('id') do
-          column("User Id")         {|user| user.email}
-          column("Affiliated School")          {|user| user.school.name}
-          column("Administrator?")  {|user| user.admin}
-        end
-      end
-    end
-  end
+#    column do
+#      panel "Current Users" do
+#        table_for User.order('id') do
+#          column("User Id")         {|user| user.email}
+#          column("Affiliated School")          {|user| user.school.name}
+#          column("Administrator?")  {|user| user.admin}
+#        end
+#      end
+#    end
+#  end
   
   end # content
 end
