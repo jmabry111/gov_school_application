db24b39b2f68203de3984dd2f5b672f2b9bc7af9
Jason Mabry <jmabry@mymabry.com>
2012-11-15 09:07:20 -0500

    refactor recommendation_creator methods and change reset password email text

--- app/models/recommendation_creator.rb
+++ app/models/recommendation_creator.rb
@@ -14,17 +14,14 @@ class RecommendationCreator
   
   def create_science_recommendation
     teacher_recommendation = create_recommendation('Science', @applicant.science_teacher_email, @applicant.science_teacher)
-    mail_recommendation(teacher_recommendation)
   end
   
   def create_math_recommendation
     teacher_recommendation = create_recommendation('Math', @applicant.math_teacher_email, @applicant.math_teacher)
-    mail_recommendation(teacher_recommendation)
   end
   
   def create_english_recommendation
     teacher_recommendation = create_recommendation('English', @applicant.english_teacher_email, @applicant.english_teacher)
-    mail_recommendation(teacher_recommendation)
   end
   
   def mail_recommendation(teacher_recommendation)
@@ -34,7 +31,7 @@ class RecommendationCreator
   def create_recommendation(subject, email, name)
     teacher_recommendation = @applicant.teacher_recommendations.new(subject: subject, email: email, teacher_name: name, last_notified_at: Time.now)
     teacher_recommendation.save(validate: false) #I'm assuming you have validations that you want to require for the teacher but enforce them now
-    teacher_recommendation #make sure we return the recommendation instead of the truth on saving
+    mail_recommendation(teacher_recommendation)
   end
   
 end
