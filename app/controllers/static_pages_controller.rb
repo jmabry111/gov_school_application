5bfbce5d9c9d461604147583f951a76d6b0f3c28
Jason Mabry <jmabry@mymabry.com>
2014-05-01 11:33:56 -0400

    allow static_pages to be viewed when registration is closed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,5 @@
 class StaticPagesController < ApplicationController
   
   skip_before_filter :authenticate_user!
-  before_filter :reject_if_registration_closed, :except => [:closed]
-  
-  
-  protected
-    def reject_if_registration_closed
-      logger.info ENV['REGISTRATION_OPEN']
-      unless ENV['REGISTRATION_OPEN'] == 'true'
-        redirect_to closed_path
-      end
-    end
+
 end
